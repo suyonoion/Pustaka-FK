8d0c0b829ab752fafd33f8c26990c8d46636e703
suyonoion <suyonoion8@gmail.com>
2026-07-19 19:47:50 +0700

    import import android.widget.ImageView

--- app/src/main/java/com/fk/arsip/MainActivity.kt
+++ app/src/main/java/com/fk/arsip/MainActivity.kt
@@ -15,6 +15,7 @@ import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.RelativeLayout
 import android.widget.TextView
