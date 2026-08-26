bbb380cadae1aaf6a7327cbbfe5281f8c718f0a8
suyonoion <suyonoion8@gmail.com>
2026-08-26 21:42:20 +0700

    eror terus

--- app/src/main/java/com/fk/arsip/MainActivity.kt
+++ app/src/main/java/com/fk/arsip/MainActivity.kt
@@ -160,7 +160,7 @@ private var kecepatanEmaBytesPerSec: Double = 0.0
         recyclerGridMode.layoutManager = GridLayoutManager(this, 2)
         sesuaikanKompartemenGrid() 
         proyektorBuku.setPageTransformer(null)
-        gridAdapter = GridAdapter(emptyList()) { posisi -> bukaModeBuku(posisi) }
+        gridAdapter = GridAdapter { posisi -> bukaModeBuku(posisi) }
         
         val layoutManagerGrid = GridLayoutManager(this, 2)
         layoutManagerGrid.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
